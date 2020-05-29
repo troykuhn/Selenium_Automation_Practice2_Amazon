4b7bdab65875388c7b0591ac83d1c7477de02779
Troy Kuhn <troykuhn22@gmail.com>
2020-05-29 11:05:41 -0400

    Add project files.

---
+++ Selenium_Automation_Practice2_Amazon/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Automation_Practice2_Amazon
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+		}
+	}
+}
