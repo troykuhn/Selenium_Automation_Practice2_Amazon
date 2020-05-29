acf43b8860ef99e52f70c7428451ae05cfda1aa9
Troy Kuhn <troykuhn22@gmail.com>
2020-05-29 11:24:17 -0400

    Testing amazon sign in failure

--- Selenium_Automation_Practice2_Amazon/Program.cs
+++ Selenium_Automation_Practice2_Amazon/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,48 @@ namespace Selenium_Automation_Practice2_Amazon
 {
 	class Program
 	{
+		// Create a reference for Chrome browser
+		IWebDriver driver = new ChromeDriver();
+
+		
+
 		static void Main( string[] args )
 		{
+
+
+		}
+
+		[SetUp]
+		public void Initialize()
+		{
+			// Go to Amazon Webpage
+			driver.Navigate().GoToUrl( "https://www.amazon.com" );
+		}
+
+
+		[Test]
+		public void ExecuteTheTest()
+		{
+			//Full screen browser
+			driver.Manage().Window.Maximize();
+
+			//Web Elements
+			IWebElement SignIn = driver.FindElement( By.Id( "nav-link-accountList" ) );
+			IWebElement EmailField = driver.FindElement( By.Id( "ap_email" ) );
+			IWebElement ContinueButton = driver.FindElement( By.Id( "continue" ) );
+
+			//Operations
+			SignIn.Click();
+			EmailField.SendKeys( "foiseoisjfosn@fwfwf.com" );
+			ContinueButton.Click();
+			
+		}
+
+		[TearDown]
+		public void CloseTest()
+		{
+			//Close the browser
+			driver.Quit();
 		}
 	}
 }
